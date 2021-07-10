c50af68cbde96c765c12e51ae7ad403a40438534
Diego Escaño <desarrolladordiegoe@gmail.com>
2021-07-10 09:27:11 -0500

    Add files via upload

---
+++ precache-manifest.2442dd7272ed8675356bb8880b25bd6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c400e150b97ccd3efba60a169190dac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffd183d88e53d6275f92",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "498e8a1a3c5c7871d7e6",
+    "url": "/static/js/2.6df052fe.chunk.js"
+  },
+  {
+    "revision": "ffd183d88e53d6275f92",
+    "url": "/static/js/main.d0a8097b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
